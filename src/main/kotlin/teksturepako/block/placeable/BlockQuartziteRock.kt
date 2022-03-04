a568c9bf960ff5762eb8f556f0aa0801edfdad72
Juraj Hrivnak <bass.jurajhrivnak@gmail.com>
2022-03-04 15:27:22 +0100

    Make placeable items only placeable when player is sneaking; Fix creative tab

--- src/main/kotlin/teksturepako/block/placeable/BlockQuartziteRock.kt
+++ src/main/kotlin/teksturepako/block/placeable/BlockQuartziteRock.kt
@@ -2,14 +2,21 @@ package teksturepako.block.placeable
 
 import net.minecraft.block.Block
 import net.minecraft.block.SoundType
+import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemBlock
+import net.minecraft.item.ItemStack
+import net.minecraft.util.EnumFacing
 import net.minecraft.util.ResourceLocation
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+import panda.divergentunderground.DivergentUnderground
 
 
 val BlockQuartziteRock : Block = object : AbstractPlaceableItem("quartzite_rock") {
 
     init {
         soundType = SoundType.STONE
+        creativeTab = DivergentUnderground.Tab
     }
 
     override val itemResourceLocation = ResourceLocation("divergentunderground", "rock_stone")
@@ -18,5 +25,17 @@ val BlockQuartziteRock : Block = object : AbstractPlaceableItem("quartzite_rock"
 val ItemBlockQuartziteRock : ItemBlock = object : ItemBlock(BlockQuartziteRock) {
     init {
         this.registryName = ResourceLocation("divergentunderground", "rock_stone")
+        creativeTab = DivergentUnderground.Tab
     }
+
+    override fun canPlaceBlockOnSide(
+        worldIn: World,
+        pos: BlockPos,
+        side: EnumFacing,
+        player: EntityPlayer,
+        stack: ItemStack
+    ): Boolean {
+        return player.isSneaking
+    }
+
 }
