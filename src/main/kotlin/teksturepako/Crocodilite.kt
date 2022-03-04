6d918fb85719c61a0df2d761e1e68f597cb43c64
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2022-03-04 20:36:27 +0100

    Bump version to 2.3

--- src/main/kotlin/teksturepako/Crocodilite.kt
+++ src/main/kotlin/teksturepako/Crocodilite.kt
@@ -42,7 +42,7 @@ import teksturepako.block.placeable.*
 object Crocodilite {
     const val MOD_ID = "crocodilite"
     const val MOD_NAME = "Crocodilite"
-    const val VERSION = "2.2"
+    const val VERSION = "2.3"
     const val DEPENDENCIES = "required-after:forgelin@[1.8.4,);required-after:divergentunderground;after:quark;after:dynamictrees"
     const val ACCEPTED_MINECRAFT_VERSIONS = "[1.12,1.12.2,)"
 
