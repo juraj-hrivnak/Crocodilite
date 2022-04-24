0f7afdf831e2f4e35a0a6f07c33e80ad35eecee6
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2022-04-24 15:14:40 +0200

    Add OvergrownSlate & OvergrownJasper blocks

--- src/main/kotlin/teksturepako/Crocodilite.kt
+++ src/main/kotlin/teksturepako/Crocodilite.kt
@@ -42,7 +42,7 @@ import teksturepako.block.placeable.*
 object Crocodilite {
     const val MOD_ID = "crocodilite"
     const val MOD_NAME = "Crocodilite"
-    const val VERSION = "2.4"
+    const val VERSION = "2.5"
     const val DEPENDENCIES =
         "required-after:forgelin@[1.8.4,);required-after:divergentunderground;after:dynamictrees;after:quark"
     const val ACCEPTED_MINECRAFT_VERSIONS = "[1.12,1.12.2,)"
@@ -76,7 +76,9 @@ object Crocodilite {
         )
         val modItemsQuark = arrayOf(
             ItemBlockOvergrownMarble,
-            ItemBlockOvergrownLimestone
+            ItemBlockOvergrownLimestone,
+            ItemBlockOvergrownSlate,
+            ItemBlockOvergrownJasper
         )
 
         // Blocks
@@ -90,9 +92,10 @@ object Crocodilite {
         )
         val modBlocksQuark = arrayOf(
             BlockOvergrownMarble,
-            BlockOvergrownLimestone
+            BlockOvergrownLimestone,
+            BlockOvergrownSlate,
+            BlockOvergrownJasper
         )
-
 
         @JvmStatic
         @SubscribeEvent
@@ -122,6 +125,8 @@ object Crocodilite {
                 if (isQuarkLoaded()) {
                     DirtHelper.registerSoil(BlockOvergrownMarble, DirtHelper.DIRTLIKE)
                     DirtHelper.registerSoil(BlockOvergrownLimestone, DirtHelper.DIRTLIKE)
+                    DirtHelper.registerSoil(BlockOvergrownSlate, DirtHelper.DIRTLIKE)
+                    DirtHelper.registerSoil(BlockOvergrownJasper, DirtHelper.DIRTLIKE)
                 }
             }
         }
@@ -160,6 +165,8 @@ object Crocodilite {
             if (isQuarkLoaded()) {
                 blockColors.registerBlockColorHandler(grassColourHandler, BlockOvergrownMarble)
                 blockColors.registerBlockColorHandler(grassColourHandler, BlockOvergrownLimestone)
+                blockColors.registerBlockColorHandler(grassColourHandler, BlockOvergrownSlate)
+                blockColors.registerBlockColorHandler(grassColourHandler, BlockOvergrownJasper)
             }
         }
 
@@ -180,6 +187,8 @@ object Crocodilite {
             if (isQuarkLoaded()) {
                 itemColors.registerItemColorHandler(itemBlockColourHandler, ItemBlockOvergrownMarble)
                 itemColors.registerItemColorHandler(itemBlockColourHandler, ItemBlockOvergrownLimestone)
+                itemColors.registerItemColorHandler(itemBlockColourHandler, ItemBlockOvergrownSlate)
+                itemColors.registerItemColorHandler(itemBlockColourHandler, ItemBlockOvergrownJasper)
             }
 
         }
--- src/main/kotlin/teksturepako/block/overgrown/QuarkOvergrownBlocks.kt
+++ src/main/kotlin/teksturepako/block/overgrown/QuarkOvergrownBlocks.kt
@@ -2,8 +2,10 @@ package teksturepako.block.overgrown
 
 import net.minecraft.block.Block
 import net.minecraft.init.Blocks
-import panda.divergentunderground.integration.QuarkCompat.HARD_LIMESTONE
 import panda.divergentunderground.integration.QuarkCompat.HARD_MARBLE
+import panda.divergentunderground.integration.QuarkCompat.HARD_LIMESTONE
+import vazkii.quark.world.feature.RevampStoneGen.slate
+import vazkii.quark.world.feature.RevampStoneGen.jasper
 
 // Prevent crash when Quark is not loaded
 fun returnBlock(name: String): Block {
@@ -11,6 +13,8 @@ fun returnBlock(name: String): Block {
         when (name) {
             "HARD_MARBLE" -> HARD_MARBLE
             "HARD_LIMESTONE" -> HARD_LIMESTONE
+            "SLATE" -> slate
+            "JASPER" -> jasper
             else -> Blocks.AIR
         }
     } catch (e: NoClassDefFoundError) {
@@ -26,3 +30,10 @@ val ItemBlockOvergrownMarble = object : AbstractOvergrownItemBlock("overgrown_ma
 val BlockOvergrownLimestone = object : AbstractOvergrown("overgrown_limestone", returnBlock("HARD_LIMESTONE")) {}
 val ItemBlockOvergrownLimestone = object : AbstractOvergrownItemBlock("overgrown_limestone", BlockOvergrownLimestone) {}
 
+// Slate
+val BlockOvergrownSlate = object : AbstractOvergrown("overgrown_slate", returnBlock("SLATE")) {}
+val ItemBlockOvergrownSlate = object : AbstractOvergrownItemBlock("overgrown_slate", BlockOvergrownSlate) {}
+
+// Jasper
+val BlockOvergrownJasper = object : AbstractOvergrown("overgrown_jasper", returnBlock("JASPER")) {}
+val ItemBlockOvergrownJasper = object : AbstractOvergrownItemBlock("overgrown_jasper", BlockOvergrownJasper) {}
